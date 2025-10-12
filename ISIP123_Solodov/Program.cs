d4755f5c8d86ceca108014648b8161f98ca1f8bb
FledBuzz896 <sobaken334@gmail.com>
2025-10-12 20:57:23 +0300

    Добавлены: Удаление студента, перевод студента на другую специальность, вывод всех студентов, вывод всех преподавателей

--- ISIP123_Solodov/Program.cs
+++ ISIP123_Solodov/Program.cs
@@ -9,10 +9,38 @@ List<Student> ThirdCourse = new List<Student>();
 List<Student> FourthCourse = new List<Student>();
 List<Student> FifthCourse = new List<Student>();
 
+int id = 1;
+string choice;
 do
 {
-    Console.WriteLine($"1. Добавить студента\n2. Удалить студента\n3. Перевести студента\n4. Добавить преподавателя");
-} while (true);
+    Console.WriteLine($"1. Добавить студента\n2. Удалить студента\n3. Перевести студента\n4. Добавить преподавателя\n5. Вывод всех преподавателей\n6. Выввод всех студегнтов\n0. Выход");
+    choice = Console.ReadLine();
+
+    switch (choice) {
+
+        case "1":
+            AddStudent(id);
+            id++;
+            break;
+        case "2":
+            DeleteStudent();
+            break;
+        case "3":
+            SwithStudent();
+            break;
+        case "4":
+            AddTeacher();
+            break;
+        case "5":
+            PrintTeachers();
+            break;
+        case "6":
+            PrintStudents();
+            break;
+        case "0": break;
+    }
+} while (choice != "0"); 
+
 
 void AddStudent(int id) {
 
@@ -31,7 +59,7 @@ void AddStudent(int id) {
     Console.Write("На какой курс поступает студент: ");
     int course = Convert.ToInt32(Console.ReadLine());
 
-    Console.Write("В какую группу добавить студента: ");
+    Console.Write("В какую группу добавить студента (123,223,323): ");
     int group = Convert.ToInt32(Console.ReadLine());
 
     Console.Write("Введите направление (ISIP_WEB, ISIP_PROG, OIBAS_WEB, OIBAS_PROG)");
@@ -59,6 +87,118 @@ void AddStudent(int id) {
         case "OIBAS_WEB": OIBAS.Add(newStudent); break;
         case "OIBAS_PROG": OIBAS.Add(newStudent); break;
     }
+    Console.WriteLine("Студент добавлден!");
+}
+void DeleteStudent() {
+
+    Console.WriteLine("Введите ID студента");
+    int id = Convert.ToInt32(Console.ReadLine());
+    string message = "Студент не найден!";
+
+    foreach (var item in ISIP)
+    {
+        if (item.GetID() == id)
+        {
+            ISIP.Remove(item);
+
+            message = "Студент удален!";        
+        }
+    }
+
+    foreach (var item in OIBAS)
+    {
+        if (item.GetID() == id)
+        {
+            OIBAS.Remove(item);
+
+            message = "Студент удален!";
+        }
+    }
+
+    Console.WriteLine(message);
+}
+void SwithStudent() {
+    
+    Console.WriteLine("введите ID студента: ");
+    int id = Convert.ToInt32( Console.ReadLine());
+    string message = "Студент не найден";
+    bool temp = true;
+
+    foreach (var item in ISIP)
+    {
+        if (item.GetID() == id)
+        {
+            ISIP.Remove(item);
+            OIBAS.Add(item);
+
+            temp = false;
+            message = "Студент переведён!";
+        }
+    }
+
+    if (temp) {
+        foreach (var item in OIBAS)
+        {
+            if (item.GetID() == id)
+            {
+                OIBAS.Remove(item);
+                ISIP.Add(item);
+
+                message = "Студент переведён!";
+            }
+        }
+    }
+    Console.WriteLine(message);
+}
+void AddTeacher() {
+
+    Console.WriteLine("введите имя");
+    string name = Console.ReadLine();
+
+    Console.WriteLine("введите возраст");
+    int age = Convert.ToInt32( Console.ReadLine());
+
+    Console.WriteLine("what is u genda?");
+    string genda = Console.ReadLine();
+
+    Console.WriteLine("В какой городе вы жрвете?");
+    string city = Console.ReadLine();
+
+    Console.WriteLine("Введите опыт работы");
+    int exp = Convert.ToInt32( Console.ReadLine());
+
+    Console.WriteLine("Введите дисциплину");
+    string sub = Console.ReadLine();
+
+    Console.WriteLine("Введите зарплату");
+    double salary = Convert.ToDouble( Console.ReadLine());
+
+    Teacher teacher = new Teacher(name, age, genda, city, exp, sub, salary);
+
+    teachers.Add(teacher);
+    Console.WriteLine("Преподаватель добавлен!");
+}
+void PrintTeachers() {
+
+    foreach (var teacher in teachers)
+    {
+        Console.WriteLine("=======================================================");
+        teacher.PrintInfo();
+    }
+    Console.WriteLine("=======================================================");
+}
+void PrintStudents() {
+
+    Console.WriteLine($"======================= 1 курс =======================");
+    foreach (var item in FirstCourse) { item.PrintInfo(); }
+    Console.WriteLine($"======================= 2 курс =======================");
+    foreach (var item in SecondCourse) { item.PrintInfo(); }
+    Console.WriteLine($"======================= 3 курс =======================");
+    foreach (var item in ThirdCourse) { item.PrintInfo(); }
+    Console.WriteLine($"======================= 4 курс =======================");
+    foreach (var item in FourthCourse) { item.PrintInfo(); }
+    Console.WriteLine($"======================= 5 курс =======================");
+    foreach (var item in FifthCourse) { item.PrintInfo(); }
 }
 
 class Person {
@@ -81,7 +221,6 @@ class Person {
         Console.WriteLine($"Имя: {name}\nВозраст: {age}\nГендер: {gender}\nГород проживания: {city}");
     }
 }
-
 class Student : Person {
 
     private int id;
@@ -108,6 +247,8 @@ class Student : Person {
         base.PrintInfo();
         Console.WriteLine($"Курс: {course}\nГруппа: {group}\nНаправление: {speciality}\nСтипендия: {grant}");
     }
+
+    public int GetID() { return id; }
 }
 class Teacher : Person {
 
