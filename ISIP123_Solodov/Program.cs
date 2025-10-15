d664e71a663d091550781ddc46305f70ccb792dd
FledBuzzard896 <sobaken334@gmail.com>
2025-10-15 15:58:35 +0300

    rgb message

--- ISIP123_Solodov/Program.cs
+++ ISIP123_Solodov/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+List<ConsoleColor> lst = new List<ConsoleColor>();
+
+lst.Add(ConsoleColor.Red);
+lst.Add(ConsoleColor.Green);
+lst.Add(ConsoleColor.Blue);
+lst.Add(ConsoleColor.Yellow);
+lst.Add(ConsoleColor.Magenta);
+lst.Add(ConsoleColor.Cyan);
+
+while (true) {
+    System.Threading.Thread.Sleep(50);
+
+    Random random = new Random();
+    int rNum = random.Next(0, lst.Count);
+
+    Console.ForegroundColor = lst[rNum];
+
+    Console.WriteLine("Привет, Мир!");
+    Console.SetCursorPosition(0, 0);
+}
+
