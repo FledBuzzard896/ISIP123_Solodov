9731a0cb70844141e0472bc0e18321c86a13a53d
FledBuzzard896 <sobaken334@gmail.com>
2025-12-03 15:52:53 +0300

    оформить вывод полной информации

--- ISIP123_Solodov_WPF/MainWindow.xaml.cs
+++ ISIP123_Solodov_WPF/MainWindow.xaml.cs
@@ -20,9 +20,79 @@ namespace ISIP123_Solodov_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        double price = 1000;
+        bool isPresent = false;
+        bool isDelivery= false;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (count != null)
+            {
+                Slider sldr = sender as Slider;
+                count.Text = $"{ sldr.Value} шт.";
+
+                price = 1000 * sldr.Value;
+
+                if (isPresent) { price += 100; }
+                if (isDelivery) { price += 200; }
+
+                total.Text = $"Итого: {price}₽";
+            }
+
+        }
+        private void box1_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            if (cb.IsChecked == true)
+            {
+                price += 100;
+                isPresent = true;
+                total.Text = $"Итого: {price}₽";
+            }
+            else 
+            {
+                price -= 100;
+                isPresent = false;
+                total.Text = $"Итого: {price}₽";
+            }
+        }
+
+        private void box2_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            if (cb.IsChecked == true)
+            {
+                price += 200;
+                isDelivery = true;
+                total.Text = $"Итого: {price}₽";
+            }
+            else if (cb.IsChecked == false)
+            {
+                price -= 200;
+                isDelivery = false;
+                total.Text = $"Итого: {price}₽";
+            }
+        }
+
+        private void TextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.Text[0]);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (name.Text == "" || phone.Text == "" || (btn1.IsChecked == false && btn2.IsChecked == false && btn3.IsChecked == false))
+            {
+                MessageBox.Show("Заполните поля!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else 
+            {
+                MessageBox.Show($"Вы: {name.Text}\nТелефон: {phone.Text}\nСпособ оплаты: {RadioButton.ContentProperty.Name}\nДополнительно: {CheckBox.ContentProperty}\nКоличество: {count.Text}\nИтог: {price} руб.");
+            }
         }
     }
 }
