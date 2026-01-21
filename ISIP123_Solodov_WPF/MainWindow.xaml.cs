4d18ec43bc228f1ff71d25d8cb3981205a1a9578
FledBuzzard896 <sobaken334@gmail.com>
2026-01-21 10:55:18 +0300

    Текстовый редактор

--- ISIP123_Solodov_WPF/MainWindow.xaml.cs
+++ ISIP123_Solodov_WPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,61 @@ namespace ISIP123_Solodov_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        string path = @"C:\Users\234885\Documents\";
+        string filePath;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            
+            if (openFileDialog.ShowDialog() == true) 
+            {
+                filePath = openFileDialog.FileName;
+
+                mainText.Text = File.ReadAllText(filePath);
+
+                this.Title = filePath;
+            }
+        }
+
+        private void SaveFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (filePath == null) 
+            {
+                SaveKAKFileBtn_Click(sender, e);
+            }
+            else 
+            {
+                File.WriteAllText(filePath, mainText.Text);
+                MessageBox.Show("Файл успешно сохранён!", "ыы бля", MessageBoxButton.OK, MessageBoxImage.Information);
+            }  
+        }
+
+        private void SaveKAKFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовый документ|*.txt";
+
+            if (saveFileDialog.ShowDialog() == true && !string.IsNullOrWhiteSpace(saveFileDialog.FileName))
+            {
+                filePath = saveFileDialog.FileName;
+
+                File.WriteAllText(filePath, mainText.Text);
+
+                this.Title = filePath;
+            }
+        }
+
+        private void CreateFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mainText.Text = "";
+            filePath = null;
+            this.Title = "TextR";
         }
     }
 }
