70d576df3a288e391058db7211c705c4bf332ad2
FledBuzzard896 <sobaken334@gmail.com>
2026-01-22 10:33:06 +0300

    Добавлены основные товары на zovб

---
+++ ISIP123_Solodov_WPF/Pages/GoodsGrid_I_like_It.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISIP123_Solodov_WPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для GoodsGrid_I_like_It.xaml
+    /// </summary>
+    public partial class GoodsGrid_I_like_It : Page
+    {
+        public GoodsGrid_I_like_It()
+        {
+            InitializeComponent();
+        }
+
+        private void MyImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Ваша логика при клике
+            MessageBox.Show("Картинка была кликнута!");
+        }
+    }
+}
