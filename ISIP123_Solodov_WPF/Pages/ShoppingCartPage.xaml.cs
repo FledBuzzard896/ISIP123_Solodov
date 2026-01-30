034f064ca6baafb79f7412adc01163434d92b01e
FledBuzz896 <sobaken334@gmail.com>
2026-01-30 18:36:04 +0300

    закончена третья страница, настроена инфраструктура с бд

--- ISIP123_Solodov_WPF/Pages/ShoppingCartPage.xaml.cs
+++ ISIP123_Solodov_WPF/Pages/ShoppingCartPage.xaml.cs
@@ -31,47 +31,56 @@ namespace ISIP123_Solodov_WPF.Pages
             if (shopCart.ak > 0)
             {
                 var res = products.Find(x => x.ProductName == "Пушка AK-47 гад дэмн");
-                text += $"Пушка AK-47 гад дэмн. Количество: {shopCart.ak}. Цена: {res.Price} * {shopCart.ak} = {res.Price * shopCart.ak}₽\n";
+                text += $"Пушка AK-47 гад дэмн. \n\tКоличество: {shopCart.ak}. Цена: {res.Price} * {shopCart.ak} = {res.Price * shopCart.ak}₽\n";
+                totalPrice += Convert.ToDouble(res.Price * shopCart.ak);
             }
             if (shopCart.belka > 0)
             {
                 var res = products.Find(x => x.ProductName == "Водка Белочка: Я пришла! 40%, 500мл");
-                text += $"Водка Белочка: Я пришла! 40%, 500мл. Количество: {shopCart.belka}. Цена: {res.Price} * {shopCart.belka} = {res.Price * shopCart.belka}₽\n";
+                text += $"Водка Белочка: Я пришла! 40%, 500мл. \n\tКоличество: {shopCart.belka}. Цена: {res.Price} * {shopCart.belka} = {res.Price * shopCart.belka}₽\n";
+                totalPrice += Convert.ToDouble(res.Price * shopCart.belka);
             }
             if (shopCart.maus > 0)
             {
                 var res = products.Find(x => x.ProductName == "Maus — легендарный сверхтяжёлый танк Второй мировой, 188 тонн");
-                text += $"Maus — легендарный сверхтяжёлый танк Второй мировой, 188 тонн. Количество: {shopCart.maus}. Цена: {res.Price} * {shopCart.maus} = {res.Price * shopCart.maus}₽\n";
+                text += $"Maus — легендарный сверхтяжёлый танк Второй мировой, 188 тонн. \n\tКоличество: {shopCart.maus}. Цена: {res.Price} * {shopCart.maus} = {res.Price * shopCart.maus}₽\n";
+                totalPrice += Convert.ToDouble(res.Price * shopCart.maus);
             }
             if (shopCart.obama > 0)
             {
                 var res = products.Find(x => x.ProductName == "Надувной Бардак Обама в полный рост");
-                text += $"Надувной Бардак Обама в полный рост. Количество: {shopCart.obama}. Цена: {res.Price} * {shopCart.obama} = {res.Price * shopCart.obama}₽\n";
+                text += $"Надувной Бардак Обама в полный рост. \n\tКоличество: {shopCart.obama}. Цена: {res.Price} * {shopCart.obama} = {res.Price * shopCart.obama}₽\n";
+                totalPrice += Convert.ToDouble(res.Price * shopCart.obama);
             }
             if (shopCart.oreshk > 0)
             {
                 var res = products.Find(x => x.ProductName == "Боевая машина поддержки танков (БМПТ) «Орешник»");
-                text += $"Боевая машина поддержки танков (БМПТ) «Орешник». Количество: {shopCart.oreshk}. Цена: {res.Price} * {shopCart.oreshk} = {res.Price * shopCart.oreshk}₽\n";
+                text += $"Боевая машина поддержки танков (БМПТ) «Орешник». \n\tКоличество: {shopCart.oreshk}. Цена: {res.Price} * {shopCart.oreshk} = {res.Price * shopCart.oreshk}₽\n";
+                totalPrice += Convert.ToDouble(res.Price * shopCart.oreshk);
             }
             if (shopCart.soyz > 0)
             {
                 var res = products.Find(x => x.ProductName == "Микрофон студийный СОЮЗ 017 TUBE");
-                text += $"Микрофон студийный СОЮЗ 017 TUBE. Количество: {shopCart.soyz}. Цена: {res.Price} * {shopCart.soyz} = {res.Price * shopCart.soyz}₽\n";
+                text += $"Микрофон студийный СОЮЗ 017 TUBE. \n\tКоличество: {shopCart.soyz}. Цена: {res.Price} * {shopCart.soyz} = {res.Price * shopCart.soyz}₽\n";
+                totalPrice += Convert.ToDouble(res.Price * shopCart.soyz);
             }
             if (shopCart.trump > 0)
             {
                 var res = products.Find(x => x.ProductName == "Надувной Дональд Трамп в полный рост");
-                text += $"Надувной Дональд Трамп в полный рост. Количество: {shopCart.trump}. Цена: {res.Price} * {shopCart.trump} = {res.Price * shopCart.trump}₽\n";
+                text += $"Надувной Дональд Трамп в полный рост. \n\tКоличество: {shopCart.trump}. Цена: {res.Price} * {shopCart.trump} = {res.Price * shopCart.trump}₽\n";
+                totalPrice += Convert.ToDouble(res.Price * shopCart.trump);
             }
             if (shopCart.ushanka > 0)
             {
                 var res = products.Find(x => x.ProductName == "Шапка ушанка енот");
-                text += $"Шапка ушанка енот. Количество: {shopCart.ushanka}. Цена: {res.Price} * {shopCart.ushanka} = {res.Price * shopCart.ushanka}₽\n";
+                text += $"Шапка ушанка енот. \n\tКоличество: {shopCart.ushanka}. Цена: {res.Price} * {shopCart.ushanka} = {res.Price * shopCart.ushanka}₽\n";
+                totalPrice += Convert.ToDouble(res.Price * shopCart.ushanka);
             }
             if (shopCart.whouse > 0)
             {
                 var res = products.Find(x => x.ProductName == "Бля Белый Дом");
-                text += $"Бля Белый Дом. Количество: {shopCart.ushanka}. Цена: {res.Price} * {shopCart.ushanka} = {res.Price * shopCart.ushanka}₽\n";
+                text += $"Бля Белый Дом. \n\tКоличество: {shopCart.whouse}. Цена: {res.Price} * {shopCart.whouse} = {res.Price * shopCart.whouse}₽\n";
+                totalPrice += Convert.ToDouble(res.Price * shopCart.whouse);
             }
 
             if (text == "")
@@ -81,16 +90,22 @@ namespace ISIP123_Solodov_WPF.Pages
             else 
             {
                 main.Text = text;
+                finalPrice.Text = $"К оплате: {totalPrice}₽";
             }
                 
         }
 
         shopCart cart = new shopCart();
         string text = "";
+        double totalPrice = 0;
 
         private void TakeOrder_Click(object sender, RoutedEventArgs e)
         {
-
+            if (main.Text != "ну ты ничего не выбрал, вернись назад бро") 
+            {
+                TakeOrderPage page = new TakeOrderPage(totalPrice);
+                NavigationService.Navigate(page);
+            }
         }
 
         private void GoBack_Click(object sender, RoutedEventArgs e)
---
+++ ISIP123_Solodov_WPF/Pages/TakeOrderPage.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISIP123_Solodov_WPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для TakeOrderPage.xaml
+    /// </summary>
+    public partial class TakeOrderPage : Page
+    {
+        public TakeOrderPage(double price)
+        {
+            InitializeComponent();
+
+            double _price = price;
+
+            final.Text += _price.ToString() + "₽";
+        }
+
+        private void payBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (name.Text != "" && mail.Text != "" && adress.Text != "")
+            {
+                Users newUser = null;
+                try 
+                {
+                    newUser = Core.Context.Users.First(u => u.Name == name.Text);
+                } catch 
+                {
+                    // Пользователь
+                    newUser = new Users
+                    {
+                        Name = name.Text,
+                    };
+
+                    Core.Context.Users.Add(newUser);
+                    Core.Context.SaveChanges();
+                } 
+               
+
+                // Заказ
+                Orders newOrder = new Orders
+                {
+                    UserID = newUser.ID,
+                    OrderDate = DateTime.Now,
+                };
+
+                Core.Context.Orders.Add(newOrder);
+                Core.Context.SaveChanges();
+
+                if (shopCart.ak > 0)
+                {
+                    Products pr = Core.Context.Products.First(p => p.ProductName == "Пушка AK-47 гад дэмн");
+                    pr.Count -= shopCart.ak;
+
+                    ProductList lst = new ProductList
+                    {
+                        ProductID = pr.ID,
+                        OrderID = newOrder.ID,
+                        Quantity = shopCart.ak,
+                    };
+
+                    Core.Context.ProductList.Add(lst);
+                    Core.Context.SaveChanges();
+                }
+
+                if (shopCart.belka > 0)
+                {
+                    Products pr = Core.Context.Products.First(p => p.ProductName == "Водка Белочка: Я пришла! 40%, 500мл");
+                    pr.Count -= shopCart.belka;
+
+                    ProductList lst = new ProductList
+                    {
+                        ProductID = pr.ID,
+                        OrderID = newOrder.ID,
+                        Quantity = shopCart.belka,
+                    };
+
+                    Core.Context.ProductList.Add(lst);
+                    Core.Context.SaveChanges();
+                }
+
+                if (shopCart.maus > 0)
+                {
+                    Products pr = Core.Context.Products.First(p => p.ProductName == "Maus — легендарный сверхтяжёлый танк Второй мировой, 188 тонн");
+                    pr.Count -= shopCart.maus;
+
+                    ProductList lst = new ProductList
+                    {
+                        ProductID = pr.ID,
+                        OrderID = newOrder.ID,
+                        Quantity = shopCart.maus,
+                    };
+
+                    Core.Context.ProductList.Add(lst);
+                    Core.Context.SaveChanges();
+                }
+
+                if (shopCart.obama > 0)
+                {
+                    Products pr = Core.Context.Products.First(p => p.ProductName == "Надувной Бардак Обама в полный рост");
+                    pr.Count -= shopCart.obama;
+
+                    ProductList lst = new ProductList
+                    {
+                        ProductID = pr.ID,
+                        OrderID = newOrder.ID,
+                        Quantity = shopCart.obama,
+                    };
+
+                    Core.Context.ProductList.Add(lst);
+                    Core.Context.SaveChanges();
+                }
+
+                if (shopCart.oreshk > 0)
+                {
+                    Products pr = Core.Context.Products.First(p => p.ProductName == "Боевая машина поддержки танков (БМПТ) «Орешник»");
+                    pr.Count -= shopCart.oreshk;
+
+                    ProductList lst = new ProductList
+                    {
+                        ProductID = pr.ID,
+                        OrderID = newOrder.ID,
+                        Quantity = shopCart.oreshk,
+                    };
+
+                    Core.Context.ProductList.Add(lst);
+                    Core.Context.SaveChanges();
+                }
+
+                if (shopCart.soyz > 0)
+                {
+                    Products pr = Core.Context.Products.First(p => p.ProductName == "Микрофон студийный СОЮЗ 017 TUBE");
+                    pr.Count -= shopCart.soyz;
+
+                    ProductList lst = new ProductList
+                    {
+                        ProductID = pr.ID,
+                        OrderID = newOrder.ID,
+                        Quantity = shopCart.soyz,
+                    };
+
+                    Core.Context.ProductList.Add(lst);
+                    Core.Context.SaveChanges();
+                }
+
+                if (shopCart.trump > 0)
+                {
+                    Products pr = Core.Context.Products.First(p => p.ProductName == "Надувной Дональд Трамп в полный рост");
+                    pr.Count -= shopCart.trump;
+
+                    ProductList lst = new ProductList
+                    {
+                        ProductID = pr.ID,
+                        OrderID = newOrder.ID,
+                        Quantity = shopCart.trump,
+                    };
+
+                    Core.Context.ProductList.Add(lst);
+                    Core.Context.SaveChanges();
+                }
+
+                if (shopCart.ushanka > 0)
+                {
+                    Products pr = Core.Context.Products.First(p => p.ProductName == "Шапка ушанка енот");
+                    pr.Count -= shopCart.ushanka;
+
+                    ProductList lst = new ProductList
+                    {
+                        ProductID = pr.ID,
+                        OrderID = newOrder.ID,
+                        Quantity = shopCart.ushanka,
+                    };
+
+                    Core.Context.ProductList.Add(lst);
+                    Core.Context.SaveChanges();
+                }
+
+                if (shopCart.whouse > 0)
+                {
+                    Products pr = Core.Context.Products.First(p => p.ProductName == "Бля Белый Дом");
+                    pr.Count -= shopCart.whouse;
+
+                    ProductList lst = new ProductList
+                    {
+                        ProductID = pr.ID,
+                        OrderID = newOrder.ID,
+                        Quantity = shopCart.whouse,
+                    };
+
+                    Core.Context.ProductList.Add(lst);
+                    Core.Context.SaveChanges();
+                }
+                MessageBox.Show("Доставка уже в пути!", "Вас заскамили", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                Application.Current.Shutdown();
+            }
+            else 
+            {
+                MessageBox.Show("Заполни все поля!!!", "Attention!!!", MessageBoxButton.OK, MessageBoxImage.Stop);
+            }
+        }
+    }
+}
