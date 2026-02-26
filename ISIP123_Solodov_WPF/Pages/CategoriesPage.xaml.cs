93b8982fff503cb64a0ef290d97c86d1b6ac6f0f
FledBuzzard896 <sobaken334@gmail.com>
2026-02-26 12:03:44 +0300

    Сделана главная страница, реализовать страничку для каждого типа товара

---
+++ ISIP123_Solodov_WPF/Pages/CategoriesPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISIP123_Solodov_WPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CategoriesPage.xaml
+    /// </summary>
+    public partial class CategoriesPage : Page
+    {
+        List<string> categories = new List<string> { "Процессоры", "Видеокарты", "Оперативная память", "Материнская плата", "Корпуса", "Блок питания", "Кулер для ЦП", "Жесткий диск/SSD" };
+        public CategoriesPage()
+        {
+            InitializeComponent();
+
+            Categories_LB.ItemsSource = categories;
+        }
+
+        void Category_Click(object sender, RoutedEventArgs e) 
+        {
+            Button btn = sender as Button;
+            string title = btn.Content.ToString();
+
+            OneProductCategoryPage page = new OneProductCategoryPage(title);
+            NavigationService.Navigate(page);
+        }
+    }
+}
---
+++ ISIP123_Solodov_WPF/Pages/OneProductCategoryPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISIP123_Solodov_WPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для OneProductCategoryPage.xaml
+    /// </summary>
+    public partial class OneProductCategoryPage : Page
+    {
+        string title;
+        public OneProductCategoryPage(string inputTitle)
+        {
+            InitializeComponent();
+
+            title = inputTitle;
+
+            Loaded += MyPageLoaded;
+        }
+
+        private void MyPageLoaded(object sender, RoutedEventArgs e) 
+        {
+            // стоп мне не приятно
+        }
+    }
+}
