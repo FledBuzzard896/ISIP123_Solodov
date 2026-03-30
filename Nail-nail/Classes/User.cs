09d2fdc4d68bdadff34241c9ffeb90afc350f065
FledBuzzard896 <sobaken334@gmail.com>
2026-03-30 12:08:37 +0300

    Сделана начальная страница. To do: - Авторизацию и регистраницию

---
+++ Nail-nail/Classes/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nail_nail.Classes
+{
+    internal static class User
+    {
+        public static string FullName { get; set; }
+        public static string PhoneNumber { get; set; }
+
+        public static string Login { get; set; }
+        public static string Password { get; set; }
+
+        public static bool isAuthorizated { get; set; } = false; 
+    }
+}
---
+++ Nail-nail/Pages/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Nail_nail.Classes;
+
+namespace Nail_nail.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Shop_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!User.isAuthorizated) 
+            {
+                MessageBox.Show("Войдите в аккаунт", "Отказано в доступе", MessageBoxButton.OK, MessageBoxImage.Stop);
+                return;
+            }
+            // Навигация на страницу
+        }
+        private void Account_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Authorization());
+        }
+        private void SignUp_ButtonClick(object sender, RoutedEventArgs e) 
+        {
+            
+        }
+    }
+}
