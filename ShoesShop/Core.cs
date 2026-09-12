3db8f5679f9ecdd63614e39a47dd6b98f2490780
FledBuzzard896 <sobaken334@gmail.com>
2026-09-12 14:37:37 +0300

    половина работы

---
+++ ShoesShop/Core.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesShop
+{
+    internal class Core
+    {
+        public static db_for_importEntities Context => new db_for_importEntities();
+
+        public static Users_ AuthUser = null;
+    }
+}
