1b1369e55fa3b9ec8f0a13c8b828ba5ec64c6320
FledBuzzard896 <sobaken334@gmail.com>
2026-09-19 14:09:24 +0300

    init commit

---
+++ pr1/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr1
+{
+    internal class Core
+    {
+        public static db_for_importEntities Context = new db_for_importEntities();
+        public static Users_ CurrentUser;
+    }
+}
